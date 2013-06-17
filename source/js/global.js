10f07827bc10ff27054b21bde659d34f62832048
Jason Semko <jasonsemko@gmail.com>
2013-06-17 15:35:00 -0700

    updating variable names and adjusting spacing

--- source/js/global.js
+++ source/js/global.js
@@ -3,40 +3,28 @@
  * @param  {Array} a [Array of anchor links within nav element]
  */
 var Parking = function(a) {
-  this.NavA = a;
+  this.navAnchors = a;
 }
 
 
-
 /**
- * Turns links blue
+ * On click listener for nav links. Upon click event page scrolls to document hash location.
  */
 Parking.prototype.scrollin = function(){
 
   var topH = document.querySelector("header").offsetHeight + 30;
-
-  this.NavA.on("click", function(e){
-
+  this.navAnchors.on("click", function(e){
     e.preventDefault();
 
     var scrollTo = ($(this.hash).position().top - topH) + "px";
-
     $("html, body").animate({ scrollTop: scrollTo });
-
   });
 }
-
 
 
 /**
  * On document ready create an instance of the Parking class and change links.
  */
 $(document).ready(function() {
-  
-  var p = new Parking(
-    $("a", "nav")
-    );
-
-  p.scrollin();
-
+  new Parking($("a", "nav")).scrollin();
 });
